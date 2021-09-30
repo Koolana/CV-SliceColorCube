caddf10cd3b5e54df67ee9cdb0fdc75d807cbff7
Nikolay Andreychik <kolya.andreychik@gmail.com>
2021-09-30 21:47:35 +0300

    Added parallel processing and comments to the code

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 import math
 import time
-import tkinter
+import multiprocessing as mp
+from multiprocessing import Pool
 
-def createCubeSlice(grayValue):
-    width = int(255 * math.sqrt(3))
-    height = int(255 * math.sqrt(3))
-    grayValue = grayValue * math.sqrt(3)
+width = int(255 * math.sqrt(3))
+height = int(255 * math.sqrt(3))
 
-    img = np.zeros([width, height, 3])
-
+# output: transformation matrix between the coordinate system
+#         of the colored cube and the slice plane with shape (3, 3)
+def getInvTransformMat():
     e1 = np.array([1, 1, 1])
     e1n = e1 / np.linalg.norm(e1)
 
@@ -25,6 +25,17 @@ def createCubeSlice(grayValue):
 
     invE = np.linalg.inv(np.array([e2n, e3n, e1n]))
 
+    return invE
+
+# input: grayValue - value of gray color intensive [0; 255]
+#        invE - transformation matrix between the coordinate system
+#               of the colored cube and the slice plane
+# output: image of the slice plane as numpy array with shape (width, height, 3)
+def createCubeSlice(grayValue, invE):
+    grayValue = grayValue * math.sqrt(3)
+
+    img = np.zeros([width, height, 3])
+
     for w in range (0, width):
         for h in range (0, height):
             vec_new = invE.dot(np.array([w - width / 2, h - height / 2, grayValue])) / 255
@@ -33,24 +44,21 @@ def createCubeSlice(grayValue):
                 vec_new[0] > 1 or vec_new[1] > 1 or vec_new[2] > 1:
                 continue
 
-            # print(vec_new)
             img[w, h, 0] = vec_new[0]
             img[w, h, 1] = vec_new[1]
             img[w, h, 2] = vec_new[2]
 
     return img
 
-def drawSlide(slideVal):
-    slideVal = int(slideVal)
-    img = createCubeSlice(slideVal)
-    cv2.imshow('Slice', img)
-    print(slideVal)
-
-
 if __name__ == "__main__":
-    # root = tkinter.Tk()
-    # tkinter.Scale(orient='horizontal', from_=0, to=255, command=drawSlide).pack()
-    drawSlide(150)
-    # root.mainloop()
+    invE = getInvTransformMat()
+
+    multiple_results = [Pool().apply_async(createCubeSlice, (grayValue, invE,)) for grayValue in range(0, 256, 5)]
+
+    time.sleep(1)
+
+    for res in multiple_results:
+        cv2.imshow('Cube slice', res.get())
+        cv2.waitKey(100)
 
     cv2.waitKey(0)
